9779335100e9a2b2191f07f5987505b6dcdc9401
ikueohira <ikue.ohira@gmail.com>
2023-01-13 14:22:42 +0100

    Edited header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 function Header() {
 	return  (
 	<div>
-		<h1>Header</h1>   
-		<p>some articles, introduction of this application</p>
+		<h1>COUNTRIES INFO</h1>   
+		<p>Här kan man veta om vilket språk eller vilken valuta 
+			används i vilket land och olika faktor om ett land.
+		</p>
+		<hr />
 	</div>
 	);
 }
