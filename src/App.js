14db217fcb5d58688cd1a7a7830680fd8d35e493
ikueohira <ikue.ohira@gmail.com>
2023-01-16 10:44:29 +0100

    Edited App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import CountryList from './components/CountryList';
-import Header from './components/Header';
-import Search from './components/Search';
-import InfoBox from './components/InfoBox';
+import "./App.css";
+import { useState, useEffect } from "react";
+import CountryList from "./components/CountryList";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import InfoBox from "./components/InfoBox";
 
-const searchCategories = ['name', 'language', 'currency'];
+const searchCategories = ["name", "language", "currency"];
 
 function App() {
-	const [countriesFilter, setCountriesFilter] = useState('');
-	const [categoriesFilter, setCategoriesFilter] = useState(searchCategories[0]);
+  const [countriesFilter, setCountriesFilter] = useState("");
+  const [categoriesFilter, setCategoriesFilter] = useState(searchCategories[0]);
+  const [selectedItem, setSelectedItem] = useState(null);
 
-	/* 
+  /* 
 	useEffect(() => {
 		console.log(countriesFilter);
 	}, [countriesFilter]);
@@ -21,18 +22,20 @@ function App() {
 	}, [categoriesFilter]);
 	*/
 
-	return (
-		<div className='App'>
-			<Header />
-			<Search
-				setCategoriesFilter={setCategoriesFilter}
-				setCountriesFilter={setCountriesFilter}
-				searchCategories={searchCategories}
-			/>
-			<CountryList />
-			<InfoBox/>
-		</div>
-	);
+  return (
+    <>
+      <div className="App">
+        <Header />
+        <Search
+          setCategoriesFilter={setCategoriesFilter}
+          setCountriesFilter={setCountriesFilter}
+          searchCategories={searchCategories}
+        />
+        <CountryList />
+      </div>
+      {selectedItem && (<InfoBox {...selectedItem}/>)}
+    </>
+  );
 }
 
 export default App;
