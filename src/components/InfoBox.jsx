3e4fd369c324829e34e17f6f34e7e06917e9be37
ikueohira <ikue.ohira@gmail.com>
2023-01-17 14:51:15 +0100

    Editied InfoBox, CSS

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -7,16 +7,18 @@ function InfoBox(selectedItem)  {
 	console.log(selectedItem);
 
 	return (
-	<div>
+	<div className='infoBox'>
 
-		<h2>{selectedItem.name}</h2>
+		<h2 className='infoBox'>{selectedItem.name}</h2>
+		
 		<table>
 			{
 				<>
-				<tr><td>{selectedItem.capital}</td></tr>
-				<tr><td>{selectedItem.population}</td></tr>
-				<tr><td>{selectedItem.area}</td></tr>
-				<tr><td>{selectedItem.flag.small}</td></tr>
+				
+				<tr><th>Capital</th> <td>{selectedItem.capital}</td></tr>
+				<tr><th>Population</th> <td>{selectedItem.population.toLocaleString()}</td></tr>
+				<tr><th>Area</th> <td>{selectedItem.area.toLocaleString()}km&sup2;</td></tr>
+				<tr><th>Flag</th> <td><img src={selectedItem.flag.small} alt={`flag of ${selectedItem.name}`} /> </td></tr>
 				</>
 			}
 		</table>
